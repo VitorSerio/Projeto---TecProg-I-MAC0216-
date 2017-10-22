1ddea5f2c57650b2a1e79cd9138faf2b230da324
VitorSerio <vitorbserio@gmail.com>
2017-10-22 02:43:12 -0200

    implementacao exercito

--- exercito.h
+++ exercito.h
@@ -4,16 +4,19 @@
 #include "robo.h"
 #include "base.h"
 
-#define MAXVM 5
+#define MAXVM 6
 
 typedef struct {
-    Robo **robos;
+    Robo *robos[MAXVM];
     Base b;
     short int size;
     short int e;
 } Exercito;
 
-Exercito *cria_exercito(INSTR **progs, short int e, int x, int y);
+Exercito *cria_exercito(INSTR *progs[], short int n, short int e,
+                        int x, int y);
 void destroi_exercito(Exercito *e);
+void adiciona_robo(Exercito *e, INSTR *p);
+void remove_robo(Exercito *e, int pos);
 
 #endif
